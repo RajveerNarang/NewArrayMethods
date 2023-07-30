fcd708b490844dddc1c68d3c65c856aa2f20b216
RajveerNarang <dhanurrajveer@gmail.com>
2023-07-30 16:57:14 +0530

    Default and Method 1

---
+++ 4NewArrayMethods/4newArrayMethods.js
@@ -0,0 +1,12 @@
+const people = ["sally", "ally", "John"];
+const peopleCopy = [...people];
+peopleCopy[2] = "New";
+console.log("copy1", peopleCopy);
+console.log("orignal1", people);
+
+// with Array method
+
+const newPeople = people.with(2, "Newer");
+
+console.log("orignal1", people);
+console.log("Newer1", newPeople);
