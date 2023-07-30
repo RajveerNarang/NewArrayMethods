8d57983386ce9ea0709c0d61bc906ee69a538954
RajveerNarang <dhanurrajveer@gmail.com>
2023-07-30 17:38:35 +0530

    ArrayMethodToSorted

--- 4NewArrayMethods/4newArrayMethods.js
+++ 4NewArrayMethods/4newArrayMethods.js
@@ -10,3 +10,9 @@ const newPeople = people.with(2, "Newer");
 
 console.log("orignal1", people);
 console.log("Newer1", newPeople);
+
+// toSorted Array method
+const sortedPeople = people.toSorted();
+
+console.log("orignal1", people);
+console.log("Sorted1", sortedPeople);
