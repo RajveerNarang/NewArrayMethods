2b707d669171ee641044bf93eafb5e92eaa55575
RajveerNarang <dhanurrajveer@gmail.com>
2023-07-31 13:35:57 +0530

    toSpliced

--- 4NewArrayMethods/4newArrayMethods.js
+++ 4NewArrayMethods/4newArrayMethods.js
@@ -1,25 +1,33 @@
 const people = ["sally", "ally", "John"];
 const peopleCopy = [...people];
 peopleCopy[2] = "New";
-console.log("copy1", peopleCopy);
+
 console.log("orignal1", people);
+console.log("copy1", peopleCopy);
 
 // with Array method
 
 const newPeople = people.with(2, "Newer");
 
-console.log("Newer1", newPeople);
 console.log("orignal1", people);
+console.log("Newer1", newPeople);
 
 // toSorted Array method
 const sortedPeople = people.toSorted();
 
-console.log("Sorted1", sortedPeople);
 console.log("orignal1", people);
+console.log("Sorted1", sortedPeople);
 
 // toReversed Array method
 
 const reversedPeople = people.toReversed();
 
-console.log("Reversed1", reversedPeople);
 console.log("orignal1", people);
+console.log("Reversed1", reversedPeople);
+
+// toSpliced Array Method
+
+const splicedPeople = people.toSpliced(2, 1, "Spliced");
+
+console.log("orignal1", people);
+console.log("Spliced1", splicedPeople);
